fc764942d523a02d7527b81e5116114fc1185e04
Barmont1341 <131329899+Barmont1341@users.noreply.github.com>
2023-11-07 10:43:46 -0600

    cambios al index

---
+++ index.js
@@ -0,0 +1,16 @@
+
+  //tabs
+  function openContent(evt, contentName) {
+      var i, x, tablinks;
+      x = document.getElementsByClassName("content");
+      for (i = 0; i < x.length; i++) {
+          x[i].style.display = "none";
+      }
+      tablinks = document.getElementsByClassName("tablink");
+      for (i = 0; i < x.length; i++) {
+          tablinks[i].classList.remove("tablink-active"); // Elimina la clase de la pestaña activa actual
+      }
+      document.getElementById(contentName).style.display = "block";
+      evt.currentTarget.firstElementChild.classList.add("tablink-active"); // Agrega la clase a la pestaña activa
+  }
+  
