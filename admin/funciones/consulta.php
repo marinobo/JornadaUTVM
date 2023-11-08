f211f2116e0879a56868ef411051f18c3a695d1d
marinobo <bothimariano2@gmail.com>
2023-11-08 11:39:07 -0600

    actualizacion

--- admin/funciones/consulta.php
+++ admin/funciones/consulta.php
@@ -1,25 +1,27 @@
 <?php
-    require 'conexion.php';
+    // Conexión a la base de datos
+    $conn = mysqli_connect('localhost', 'root', '', 'jornadaUTVM');
 
-    if (!$mysqli) {
-        # code...
-        die('NO SE PUEDE CONECTAR A BASE DE DATOS');
+    // Recuperar los datos del formulario de inicio de sesión
+    $usuario = $_POST['usuario'];
+    $pass = $_POST['pass'];
+
+    // Consulta para verificar las credenciales del usuario
+    $query = "SELECT * FROM loginAdmin WHERE usuario='$usuario' AND pass='$pass'";
+    $result = mysqli_query($conn, $query);
+
+    // Verificar si se encontraron coincidencias en la base de datos
+    if (mysqli_num_rows($result) == 1) {
+        // Credenciales válidas, iniciar sesión
+        session_start();
+        $_SESSION['username'] = $username;
+        // Redirigir al usuario a la página de inicio
+        header('Location: index.html');
+    } else {
+        // Credenciales inválidas, mostrar mensaje de error
+        echo "Nombre de usuario o contraseña incorrectos.";
     }
 
-    $usuario = utf8_decode($_POST['usuario']);
-    $pass = utf8_decode($_POST['pass']);
-    $contodar =0;
-
-    if (isset($_POST["btniniciar"])) {
-        # code...
-        if (password_verify($password, $query['pass'] )){
-            $contodar++;
-        }
-        $query = mysqli_query($mysqli, "SELECT * FROM loginAdmin¡ WHERE usuario = '$usuario' AND pass = '$pass'");
-        $nr = mysqli_num_rows($query);
-        if ($nr==1) {
-            echo "<script> alert('Bienvenido: $usuario'); window.location='index.html'</script>";
-        }else {
-            echo "<script> alert('El usuario no existe'); window.location='login.html'</script>";
-        }
-    }
+    // Cerrar la conexión a la base de datos
+    mysqli_close($conn);
+?>
