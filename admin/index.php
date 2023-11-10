6590cf64303d1748e5a646b9142b4e2abba302e3
marinobo <bothimariano2@gmail.com>
2023-11-10 08:25:24 -0600

    8:25

--- admin/index.php
+++ admin/index.php
@@ -9,8 +9,8 @@
 <body>
     <form action="validar.php" method="post">
         <h1>Iniciar Sesión</h1>
-        <p>Usuario <input type="text" placeholder="Ingrese su Correo" name="usuario"></p>
-        <p>Contraseña <input type="password" placeholder="Ingrese su contraseña" name="pass"></p>
+        <p>Usuario <input type="text" placeholder="Ingrese su Correo" name="usuario" require></p>
+        <p>Contraseña <input type="password" placeholder="Ingrese su contraseña" name="pass" require></p>
         <input type="submit" value="Ingresar">
     </form>
 </body>
